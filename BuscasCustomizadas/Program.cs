008600b4094d0351b47348d6c7395766cdce6003
Yan Paulo <yanpaulo@hotmail.com>
2017-12-18 12:28:32 -0300

    Projeto para testes.

---
+++ BuscasCustomizadas/Program.cs
@@ -0,0 +1,41 @@
+﻿using BuscaBidirecional;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscasCustomizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mapa = Mapa.FromFile();
+            var problema = new ProblemaMapa
+            {
+                Mapa = mapa,
+                Origem = mapa.Locais.Single(l => l.Nome == "Oradea"),
+                Destino = mapa.Locais.Single(l => l.Nome == "Timisoara")
+            };
+
+            var algoritmo = new BuscaAprofundamentoIterativo<Local>(problema);
+            while (!algoritmo.AtingiuObjetivo && !algoritmo.Falha)
+            {
+                Console.WriteLine(algoritmo.ImprimeListas());
+                algoritmo.Expande();
+            }
+
+            if (algoritmo.Falha)
+            {
+                Console.WriteLine("Falha!");
+            }
+            else
+            {
+                Console.WriteLine(algoritmo.ImprimeCaminho());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
