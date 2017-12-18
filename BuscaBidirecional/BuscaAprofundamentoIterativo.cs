0081805ad29f7079e3be92e5734b96597696167f
Yan Paulo <yanpaulo@hotmail.com>
2017-12-18 11:34:03 -0300

    Aprofundamento Iterativo.

---
+++ BuscaBidirecional/BuscaAprofundamentoIterativo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscaBidirecional
+{
+    public class BuscaAprofundamentoIterativo<T> : IAlgoritmo<T>
+    {
+        private BuscaEmProfundidadeArvore<T> algoritmoProfundidade;
+        
+        public BuscaAprofundamentoIterativo(IProblema<T> problema)
+        {
+            Problema = problema;
+            algoritmoProfundidade = new BuscaEmProfundidadeArvore<T>(problema, Limite);
+        }
+
+        public int Limite { get; private set; } = 0;
+
+        public IProblema<T> Problema { get; private set; }
+
+        public No<T> Objetivo =>
+            algoritmoProfundidade.Objetivo;
+
+        public IEnumerable<No<T>> Borda =>
+            algoritmoProfundidade.Borda;
+
+        public bool Falha =>
+            false;
+
+        public bool AtingiuObjetivo =>
+            Objetivo != null;
+
+        public void Expande()
+        {
+            if (algoritmoProfundidade.Falha)
+            {
+                algoritmoProfundidade = new BuscaEmProfundidadeArvore<T>(Problema, ++Limite);
+            }
+            else
+            {
+                algoritmoProfundidade.Expande();
+            }
+        }
+
+        public string ImprimeListas() =>
+            algoritmoProfundidade.ImprimeListas();
+    }
+}
